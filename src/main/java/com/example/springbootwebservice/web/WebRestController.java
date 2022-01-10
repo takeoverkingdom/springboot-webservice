d215cf79553cc4bde7c23189dcb4883794760c8a
js.kim <js.kim@koreacb.com>
2022-01-10 17:31:41 +0900

    commit

---
+++ src/main/java/com/example/springbootwebservice/web/WebRestController.java
@@ -0,0 +1,14 @@
+package com.example.springbootwebservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "helloworld";
+    }
+
+}
